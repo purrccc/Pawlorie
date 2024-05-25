7ffd8c3d6d60c89b6b50b8b91f2779041c03d42f
Maxinne11 <97075396+Maxinne11@users.noreply.github.com>
2024-05-25 13:03:20 +0800

    added minor changes in font in HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,71 +2,69 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pawlorie/AddDogPage.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pawlorie/constants/colors.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Stack(
         children: [
           Column(
-            children: [       
-                Container(
-                  height: 150,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
+            children: [
+              Container(
+                height: 150,
+                width: double.infinity,
+                decoration: const BoxDecoration(
                   color: Color.fromARGB(255, 22, 21, 86),
                   borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(30),
                     bottomRight: Radius.circular(30),
                   ),
                 ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                         children: [
-                           Text(
-                             'Hello, ',
-                              style: TextStyle(fontSize: 30, color: Colors.white),
-                            ),
-                            Text('User!',
-                              style: TextStyle(
-                                fontSize: 34,
-                                fontFamily: 'Rubik',
-                                fontWeight: FontWeight.w800,
-                                color: Color.fromARGB(255, 222, 152, 32),
-                              ),
-                            ),
-                         ],
-                       ),
-              
-                      SizedBox(height: 8),
-                      Text(
-                        "Insert Date",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Hello, ',
+                          style: GoogleFonts.rubik(
+                              fontSize: 31,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
-                      ),
-                    ],
-                  ),
+                        Text(
+                          'User!',
+                          style: GoogleFonts.rubik(
+                              fontSize: 34,
+                              fontWeight: FontWeight.w800,
+                              color: AppColor.yellowGold),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "Insert Date",
+                      style:
+                          GoogleFonts.ubuntu(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
                 ),
-              
+              ),
             ],
           ),
-          const Positioned(
-            top: 180, 
+          Positioned(
+            top: 180,
             left: 16,
             child: Text(
               'Your dogs',
-              style: TextStyle(
-                color: Color.fromARGB(255, 22, 21, 86),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+              style: GoogleFonts.rubik(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: AppColor.darkBlue),
             ),
           ),
           Positioned(
@@ -76,9 +74,7 @@ class HomePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                child: buildDogContainer( 
-                "name","breed"),
-              
+                child: buildDogContainer("name", "breed"),
               ),
             ),
           ),
@@ -90,48 +86,58 @@ class HomePage extends StatelessWidget {
               height: 100,
               width: double.infinity,
               decoration: const BoxDecoration(
-                 color: Color.fromARGB(255, 31, 104, 239),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(100),
-                      topRight: Radius.circular(100),
-                    ),
-              ), 
+                color: Color.fromARGB(255, 31, 104, 239),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                  topRight: Radius.circular(100),
+                ),
+              ),
             ),
           ),
-    Positioned(
-  bottom: 60, 
-  left: 0,
-  right: 0,
-  child: Center(
-    child: ElevatedButton(
-        onPressed: () => Navigator.push(context, 
-                            MaterialPageRoute(
-                              builder: (context) => AddDogPage())
-                              ), // anonymous route
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 255, 202, 27),
-        padding: const EdgeInsets.all(20),
-      ),
-      child: const Text(
-        'Add Dog', 
-        style: TextStyle(
-          color: Color.fromARGB(255, 22, 21, 86),
-          fontSize: 18,
-          fontWeight: FontWeight.w800, 
-        ),
-      ),
-    ),
-  ),
-),
+          Positioned(
+            bottom: 60,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddDogPage())), // anonymous route
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 255, 202, 27),
+                  padding: const EdgeInsets.all(20),
+                ),
+                child: Row(
+                  mainAxisSize:
+                      MainAxisSize.min, // Ensure the button fits its content
+                  children: [
+                    Icon(
+                      Icons.add, // The plus icon
+                      color: Color.fromARGB(255, 22, 21, 86),
+                    ),
+                    SizedBox(width: 8), // Space between the icon and the text
+                    Text(
+                      'Add Dog',
+                      style: GoogleFonts.ubuntu(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        color: Color.fromARGB(255, 22, 21, 86),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-
 // Function to display dogs
-Widget buildDogContainer( String name, String breed) {
+Widget buildDogContainer(String name, String breed) {
   return Container(
     decoration: BoxDecoration(
       color: Colors.white,
@@ -141,7 +147,7 @@ Widget buildDogContainer( String name, String breed) {
           color: Colors.grey.withOpacity(0.5),
           spreadRadius: 2,
           blurRadius: 5,
-          offset: Offset(0, 3), 
+          offset: Offset(0, 3),
         ),
       ],
     ),
@@ -152,12 +158,12 @@ Widget buildDogContainer( String name, String breed) {
           height: 180,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
-            color: Colors.black,),
-          
+            color: Colors.black,
+          ),
         ),
         Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Column( 
+          child: Column(
             children: [
               Text(
                 name,
