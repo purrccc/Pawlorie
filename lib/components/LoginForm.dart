e2339ea0c6a1561ae320257bcc3295f3389b156f
purrccc <persecatalan@gmail.com>
2024-05-28 16:34:49 +0800

    gagana na ang calories and form

--- lib/components/LoginForm.dart
+++ lib/components/LoginForm.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pawlorie/SignupPage.dart'; // Adjust this import based on your file structure
+import 'package:pawlorie/SignupPage.dart'; 
 
 class LoginForm extends StatelessWidget {
   final GlobalKey<FormState> formKey;
